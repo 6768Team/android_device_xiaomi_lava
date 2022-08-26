c6192d4769387d4756f05403a0a0987ad223aa08
Matsvei Niaverau <matvej2002@gmail.com>
2022-08-26 11:59:24 +0300

    lava: Convert overlays into Runtime Resource Overlays
    Change-Id: I7d41a51799f1f731e2f0584c43128944da5a38ef

--- device.mk
+++ device.mk
@@ -30,8 +30,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/aurisys_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/aurisys_config.xml
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayLava
 
 # Kernel
 PRODUCT_COPY_FILES += \
